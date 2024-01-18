638189df21c921f75e602d864ca9315ae64f584d
honx sinatra <josephhongoa2020@gmail.com>
2024-01-18 09:14:07 +0300

    Developing RiseFundFor component

--- Components/RiseFundFor.jsx
+++ Components/RiseFundFor.jsx
@@ -4,24 +4,16 @@ import React from "react";
 const RiseFundFor = () => {
   return (
     <div className="RFFConrainer">
-      <Link href={""} className="howButton">
-        How it works
-      </Link>
-      <section>
-        <div>
-          <span>Rise fund for</span>
-          <i class="fa-solid fa-angle-down"></i>
-        </div>
-        <ul>
-          <li>Charity</li>
-          <li>Project</li>
-          <li>Event/Party</li>
-          <li>DisasterRelief</li>
-          <li>SpaceResearch</li>
-          <li>MedicalAid</li>
-          <li>Other</li>
-        </ul>
-      </section>
+      <h2>Rise fund for</h2>
+      <ul>
+        <li>Charity</li>
+        <li>Project</li>
+        <li>Event/Party</li>
+        <li>DisasterRelief</li>
+        <li>SpaceResearch</li>
+        <li>MedicalAid</li>
+        <li>Other</li>
+      </ul>
     </div>
   );
 };
