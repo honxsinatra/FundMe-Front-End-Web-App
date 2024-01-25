f590db75cf929884cbd101f71d932d86d84ebe52
honx sinatra <josephhongoa2020@gmail.com>
2024-01-25 23:39:34 +0300

    Developing WelcomePage component

--- Components/WelcomePage.jsx
+++ Components/WelcomePage.jsx
@@ -1,46 +1,57 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import Nav from "./Nav";
 import RiseFundFor from "./RiseFundFor";
 import Image from "next/image";
 import buku from "../public/images/buku.jpg";
 import raiser from "../public/images/raiser.jpg";
+import metamask from "../public/images/metamask.png";
+import { ethers } from "ethers";
+import { Web3Provider } from "ethers/providers";
+//import { abi, contractAddress } from "../constants.js";
 
 const WelcomePage = () => {
-  const statements = [
-    "Your Blockchain-Powered Decentralized Application",
-    "Want to raise funds?",
-    "Here is your solution",
-  ];
-
-  const [currentStatement, setCurrentStatement] = useState(0);
-  const [currentText, setCurrentText] = useState("");
-
   useEffect(() => {
-    if (currentStatement < statements.length) {
-      const intervalId = setInterval(() => {
-        const text = statements[currentStatement].substring(
-          0,
-          currentText.length + 1
-        );
-        setCurrentText(text);
+    const connectButton = document.getElementById("connectButton");
+    const connectLogs = document.getElementById("connectLogs");
 
-        if (text === statements[currentStatement]) {
-          clearInterval(intervalId);
+    connectButton.onclick = connect;
 
-          // Move to the next statement after a delay
-          setTimeout(() => {
-            setCurrentStatement(currentStatement + 1);
-            setCurrentText("");
-          }, 1000);
-        }
-      }, 100);
-
-      return () => clearInterval(intervalId);
+    function logMessage(logType, message) {
+      const logElement = document.createElement("div");
+      logElement.textContent = message;
+      logType.appendChild(logElement);
     }
-  }, [currentStatement, currentText, statements]);
+  });
 
+  // Check if Metamask is installed
+  async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+      // Initialize Metamask provider
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+      // Request access to the user's Ethereum account
+      await ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((accounts) => {
+          const userAddress = accounts[0];
+          logMessage(
+            connectLogs,
+            `Connected to Metamask with address: ${userAddress}`
+          );
+
+          // You can now use `provider` to interact with the Ethereum blockchain
+          // For example, you can use it to send transactions or query contract data.
+        })
+        .catch((error) => {
+          logMessage(connectLogs, "Error connecting to Metamask:" + error);
+        });
+      connectButton.innerHTML = "Connected";
+    } else {
+      console.error("Metamask is not installed");
+    }
+  }
   return (
     <div className="welcomeContainer">
       <div className="left-side">
@@ -52,8 +63,14 @@ const WelcomePage = () => {
       </div>
       <div className="right-side">
         <div className="right-container">
+          <div className="connect">
+            Please connect to Metamask{" "}
+            <button id="connectButton">Connect</button>
+            <Image src={metamask} alt="metamask icon" />
+          </div>
+          <div id="connectLogs" class="arrangement"></div>
           <h1>
-            Welcome to <span className="fundraiser">FUNDRAISER</span> –
+            Welcome to <span className="fundraiser">FUNDRAISER</span>
             Empowering Change with Transparency through Blockchain technology!
           </h1>
           <div className="images">
@@ -75,12 +92,6 @@ const WelcomePage = () => {
             trust with donors through transparent transactions. As a donor, you
             can track how your contribution has been utilized.
           </p>
-          <div className="typing-container">
-            <div className="typing-text">
-              {currentText}
-              <span className="blink">|</span>
-            </div>
-          </div>
         </div>
       </div>
     </div>
