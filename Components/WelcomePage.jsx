62f5eef5e1a7b205e21bcca331e58ec817f47016
honx sinatra <josephhongoa2020@gmail.com>
2024-02-19 19:49:32 +0300

    progress

--- Components/WelcomePage.jsx
+++ Components/WelcomePage.jsx
@@ -6,20 +6,34 @@ import buku from "../public/images/buku.jpg";
 import raiser from "../public/images/raiser.jpg";
 import metamask from "../public/images/metamask.png";
 import "@rainbow-me/rainbowkit/styles.css";
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { http, createConfig, WagmiProvider } from "wagmi";
 import { sepolia, goerli, polygonMumbai } from "wagmi/chains";
-import { alchemyProvider } from "wagmi/providers/alchemy";
-import { publicProvider } from "wagmi/providers/public";
 import { CustomConnectButton } from "./CustomConnectButton";
+import { injected, safe, walletConnect } from "wagmi/connectors";
 //import { abi, contractAddress } from "../constants.js";
 
 const WelcomePage = () => {
-  const { chains, publicClient } = configureChains(
-    [sepolia, goerli, polygonMumbai],
-    [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
-  );
-  const { connectors } = getDefaultWallets({
+  const projectId = process.env.PROJECT_ID;
+
+  const queryClient = new QueryClient();
+
+  const config = createConfig({
+    chains: [sepolia, goerli, polygonMumbai],
+    connectors: [
+      injected({ target: "metaMask" }),
+      walletConnect({ projectId }),
+      safe(),
+    ],
+    transports: {
+      [sepolia.id]: http("https://eth-sepolia.g.alchemy.com/v2/demo"),
+      [goerli.id]: http("https://eth-goerli.g.alchemy.com/v2/demo"),
+      [polygonMumbai.id]: http("https://polygon-mumbai.g.alchemy.com/v2/demo"),
+    },
+  });
+
+  /*const { connectors } = getDefaultWallets({
     appName: "FundRaiser DApp",
     projectId: "process.env.PROJECT_ID",
     chains,
@@ -28,56 +42,58 @@ const WelcomePage = () => {
     autoConnect: true,
     connectors,
     publicClient,
-  });
+  });*/
 
   return (
-    <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider coolMode chains={chains}>
-        <div className="welcomeContainer">
-          <div className="left-side">
-            <div className="fixed">
-              <Nav />
-              <RiseFundFor />
-              <footer>&copy; 2024 Hongoa. All Rights Reserved.</footer>
-            </div>
-          </div>
-          <div className="right-side">
-            <div className="right-container">
-              <div className="connect">
-                Please connect to Metamask <CustomConnectButton />
-                <Image src={metamask} alt="metamask icon" />
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider coolMode chains={chains}>
+          <div className="welcomeContainer">
+            <div className="left-side">
+              <div className="fixed">
+                <Nav />
+                <RiseFundFor />
+                <footer>&copy; 2024 Hongoa. All Rights Reserved.</footer>
               </div>
-              <div id="connectLogs" className="arrangement"></div>
-              <h1>
-                Welcome to <span className="fundraiser">FUNDRAISER</span>
-                Empowering Change with Transparency through Blockchain
-                technology!
-              </h1>
-              <div className="images">
-                <Image
-                  src={buku}
-                  alt="An old man holding Tshs.1000/="
-                  loading="lazy"
-                />
-                <Image
-                  src={raiser}
-                  alt="Woman's hand holding dollars"
-                  loading="lazy"
-                />
-              </div>
-              <p>
-                Join our mission for positive impact through transparent
-                fundraising, emphasizing openness, accountability, and the power
-                of collective action. Use this platform to raise funds,
-                establishing trust with donors through transparent transactions.
-                As a donor, you can track how your contribution has been
-                utilized.
-              </p>
             </div>
-          </div>
-        </div>{" "}
-      </RainbowKitProvider>
-    </WagmiConfig>
+            <div className="right-side">
+              <div className="right-container">
+                <div className="connect">
+                  Please connect to Metamask <CustomConnectButton />
+                  <Image src={metamask} alt="metamask icon" />
+                </div>
+                <div id="connectLogs" className="arrangement"></div>
+                <h1>
+                  Welcome to <span className="fundraiser">FUNDRAISER</span>
+                  Empowering Change with Transparency through Blockchain
+                  technology!
+                </h1>
+                <div className="images">
+                  <Image
+                    src={buku}
+                    alt="An old man holding Tshs.1000/="
+                    loading="lazy"
+                  />
+                  <Image
+                    src={raiser}
+                    alt="Woman's hand holding dollars"
+                    loading="lazy"
+                  />
+                </div>
+                <p>
+                  Join our mission for positive impact through transparent
+                  fundraising, emphasizing openness, accountability, and the
+                  power of collective action. Use this platform to raise funds,
+                  establishing trust with donors through transparent
+                  transactions. As a donor, you can track how your contribution
+                  has been utilized.
+                </p>
+              </div>
+            </div>
+          </div>{" "}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 };
 
